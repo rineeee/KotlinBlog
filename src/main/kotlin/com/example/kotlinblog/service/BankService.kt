3d4bd6838332a372904ee9ea3be48b5cc6617ec9
rineeee <harin14@naver.com>
2022-12-24 17:34:44 +0900

    feat : add service layer

--- src/main/kotlin/com/example/kotlinblog/service/BankService.kt
+++ src/main/kotlin/com/example/kotlinblog/service/BankService.kt
@@ -1,8 +1,12 @@
 package com.example.kotlinblog.service
 
+import com.example.kotlinblog.datasource.BankDataSource
+import com.example.kotlinblog.model.Bank
 import org.springframework.stereotype.Service
 
 @Service
-class BankService {
+class BankService(private val dataSource: BankDataSource) {
+    fun getBanks(): Collection<Bank> = dataSource.retrieveBanks()
+
 
 }
