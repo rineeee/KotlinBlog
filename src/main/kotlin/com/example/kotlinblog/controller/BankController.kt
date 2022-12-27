9d2c93acfac2fa0c4495df782e3fbc35a4036cef
rineeee <harin14@naver.com>
2022-12-27 17:10:35 +0900

    feat : /api/banks api

--- src/main/kotlin/com/example/kotlinblog/controller/BankController.kt
+++ src/main/kotlin/com/example/kotlinblog/controller/BankController.kt
@@ -1,7 +1,15 @@
 package com.example.kotlinblog.controller
 
+import com.example.kotlinblog.model.Bank
+import com.example.kotlinblog.service.BankService
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class BankController {
+@RequestMapping("/api/banks")
+class BankController(private val service: BankService) {
+
+    @GetMapping
+    fun getBanks(): Collection<Bank> = service.getBanks()
 }
